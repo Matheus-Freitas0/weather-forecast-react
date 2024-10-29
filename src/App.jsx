d64e1395649b359866612bb7b47035945a30d724
MatheusFreitas <moncaomatheus@gmail.com>
2024-10-29 17:33:30 -0300

    Updated index.html title, added axios dependency, and modified App.jsx to fetch weather data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState, useRef } from 'react'
-
-import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [weather, setWeather] = useState({})
   const inputRef = useRef()
 
-  function searchCity() {
-    console.log(inputRef.current.value)
+  async function searchCity() {
+    
+    const city = inputRef.current.value
+    const key = "8aa46f48d922e668583a007f2503f1f1"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`
+    const apiDataWeather = await axios.get(url)
+    setWeather(apiDataWeather.data)
+
+    console.log(apiDataWeather.data)
   }
 
   return (
