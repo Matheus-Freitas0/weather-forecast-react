8cd8b8ba3c89b43673785ee4bda0216762f58d14
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-10-30 15:37:36 -0300

    Updated App.css with new styles and added CSS file for WeatherInformations component with corresponding class names

--- src/components/WeatherInformations/WeatherInformations.jsx
+++ src/components/WeatherInformations/WeatherInformations.jsx
@@ -1,17 +1,19 @@
+import './WeatherInformations.css'
+
 function WeatherInformations({ weather }) {
 
   return (
-    <div>
+    <div className='weather-container'>
       <h2>{weather.name}</h2>
-      <div>
+      <div className='weather-info'>
         <img
           src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
           alt={weather.weather[0].description}
         />
-        <p>{Math.round(weather.main.temp)}°C</p>
+        <p className='temperature'>{Math.round(weather.main.temp)}°C</p>
       </div>
-      <p>{weather.weather[0].description}</p>
-      <div>
+      <p className='description'>{weather.weather[0].description}</p>
+      <div className='details'>
         <p>Sensação térmica: {Math.round(weather.main.feels_like)}°C</p>
         <p>Umidade: {weather.main.humidity}%</p>
         <p>Pressão: {weather.main.pressure} hPa</p>
